d360d23a526f2f1f0ef38e8d39c54b0670eb7a65
anqingsong101 <32869738+anqingsong101@users.noreply.github.com>
2017-10-17 19:57:42 +0800

    Add files via upload

---
+++ shiX/aChajian/server.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var fs = require("fs");
+http.createServer(function(req,res) {
+	res.setHeader("Access-Control-Allow-Origin","*")
+	fs.readFile(".json",function(err,data) {
+		if (err) {
+			res.write("404");
+			console.log(err)
+		} else{
+			res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+			res.write(data);
+		}
+		res.end()
+	})
+}).listen(1100)
+console.log("0")
